ecadf03cd8ee102cfa7c36d18c9674e63327d2f5
Polisetty Sathvik <polisettysathvik@gmail.com>
2026-06-28 08:35:58 +0530

    Remove comments from futureValue method

--- WEEK-1/DSA/EX-7/FinancialForecast.java
+++ WEEK-1/DSA/EX-7/FinancialForecast.java
@@ -1,6 +1,5 @@
 public class FinancialForecast {
 
-    // Recursive method to calculate future value
     public static double futureValue(double currentValue, double growthRate, int years) {
 
         // Base case
@@ -8,7 +7,6 @@ public class FinancialForecast {
             return currentValue;
         }
 
-        // Recursive call
         return futureValue(currentValue * (1 + growthRate), growthRate, years - 1);
     }
 }
